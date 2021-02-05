5266f1efff4935d4f5529bbe77fbb3e003e29a56
Kyle G <legokyle@frontier.com>
2021-02-04 18:26:52 -0800

    Pushing before adding to heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+// Required items
 const fs = require('fs');
 const path = require('path');
 const genID = require('generate-unique-id');
@@ -6,40 +7,46 @@ require('./htmlRoutes');
 
 module.exports = app => {
 
+    // Will be used to write to the DB
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
 
         let notesArray = JSON.parse(data);
 
+        // Used to get the route for our api
         app.get('/api/notes', (req, res) => {
             res.json(notesArray);
-        });
+        })
 
+        // Used to create new note and push it to the DB
         app.post('/api/notes', (req, res) => {
             const addNoteArray = {
-                title: req.body.title,
-                text: req.body.text,
                 id: genID({
-                    length: 10,
+                    length: 5,
                     useLetters: false
-                })
+                }),
+                title: req.body.title,
+                text: req.body.text
             }
             notesArray.push(addNoteArray);
-            res.json(notesArray);
             updateNoteDB(notesArray);
+            res.json(notesArray);
             console.log(`A note has been added to the array titled: ${addNoteArray.title}`);
-        });
+        })
 
+        // Used to get note by ID
         app.get('/api/notes/:id', (req, res) => {
             res.json(notesArray[req.params.id]);
-        });
+        })
 
+        // Used to delete notes (broken, and im not sure why)
         app.delete('/api/notes/:id', (req, res) => {
+            console.log(req.params.id);
             notesArray.splice(req.params.id, 1);
             updateNoteDB(notesArray);
             res.json(notesArray);
             console.log(`A note with an id of ${req.params.id} has been deleted`);
-        });
+        })
     });
 
     const updateNoteDB = (notesArray) => {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,20 @@
 const path = require('path');
 
 module.exports = (app) => {
+    // Used to set our home page
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-
+    // Used to set our notes page
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-
+    // Used to display our DB on the webpage
     app.get('/api/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../db/db.json'));
     });
-
+    // Will return to the home screen if anything else is entered
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.//html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
